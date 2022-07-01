d68dab6f3bca38ab9cba94589c3d725c59b7e344
DanaRizoPalacios <86388506+DanaRizoPalacios@users.noreply.github.com>
2022-06-30 20:45:22 -0600

    Version Semana 4 proyecto en clase (Actualizar lugares)

--- app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
@@ -8,7 +8,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lugares.R
+import com.lugares.adapter.LugarAdapter
 import com.lugares.databinding.FragmentLugarBinding
 import com.lugares.viewmodel.LugarViewModel
 
@@ -25,7 +27,23 @@ class LugarFragment : Fragment() {
             ViewModelProvider(this).get(LugarViewModel::class.java)
         _binding = FragmentLugarBinding.inflate(inflater, container, false)
         binding.addLugar.setOnClickListener {
-            findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)  }
+            findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)
+        }
+
+// Activar reciclador
+        val lugarAdapter=LugarAdapter()
+        val reciclador= binding.reciclador
+        reciclador.adapter = lugarAdapter
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+        lugarViewModel.getAllData.observe(viewLifecycleOwner){
+            lugarAdapter.setData(it)
+        }
+
+
+
+
+
+
         return binding.root
     }
     override fun onDestroyView() {
